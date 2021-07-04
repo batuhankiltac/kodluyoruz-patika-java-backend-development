e4d633c52f3f83a5d397ca95a5fe8277f8360d9f
Batuhan Kıltaç <81926452+lbakteri@users.noreply.github.com>
2021-07-04 21:56:51 +0300

    Update AccountManager.java

--- JAVA102-SigortaYonetimSistemi/src/AccountManager.java
+++ JAVA102-SigortaYonetimSistemi/src/AccountManager.java
@@ -3,16 +3,16 @@ import java.util.*;
 public class AccountManager {
     TreeSet<Account> accounts = new TreeSet<>();
 
-    public Account login(String email,String sifre) throws InvalidAuthenticationException {
-        for (Account a : accounts) {
-            if (a.user.email.equals(email) && a.user.sifre.equals(sifre)) {
+    public Account login(String email, String sifre) throws InvalidAuthenticationException {
+        for (Account account : accounts) {
+            if (account.user.email.equals(email) && account.user.sifre.equals(sifre)) {
                 try {
-                    a.login(email,sifre);
+                    account.login(email, sifre);
                 }
                 catch (Error e) {
                     System.out.println(e.getMessage());
                 }
-                return a;
+                return account;
             }
         }
         return null;
