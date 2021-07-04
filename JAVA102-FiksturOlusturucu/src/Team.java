2b2a2196e8e594ae52861342c7da9c0c7416f04b
Batuhan Kıltaç <81926452+lbakteri@users.noreply.github.com>
2021-07-04 12:56:09 +0300

    Add files via upload

---
+++ JAVA102-FiksturOlusturucu/src/Team.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Team {
+    String name;
+
+    ArrayList<String> opponent = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
---
+++ JAVA102-FiksturOlusturucu/src/Week.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Week {
+    int number;
+
+    ArrayList<String> homeTeam = new ArrayList<>();
+    ArrayList<String>  awayTeam = new ArrayList<>();
+
+    public Week(int number) {
+        this.number = number;
+    }
+}
