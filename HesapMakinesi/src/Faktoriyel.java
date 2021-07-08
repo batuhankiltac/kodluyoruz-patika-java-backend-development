defac95f7ef390a09e5a780c0793452d11f5a95c
Batuhan Kıltaç <81926452+lbakteri@users.noreply.github.com>
2021-07-08 17:15:43 +0300

    Add files via upload

---
+++ HesapMakinesi/src/Faktoriyel.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Faktoriyel {
+
+    static void faktoriyel() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Faktöriyel alınacak sayıyı giriniz: ");
+        int n = input.nextInt();
+
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        System.out.print(n + " faktöriyel sonucu: " + result + "\n");
+    }
+}
