cff2ed879c83f18b856823f60ae56d0b59b39f62
Batuhan Kıltaç <81926452+batuhankiltac@users.noreply.github.com>
2021-12-28 15:20:00 +0300

    Delete JAVA102-PatikaStore directory

--- JAVA102-PatikaStore/src/BrandNameComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class BrandNameComparator implements Comparator<Brand> {
-
-    @Override
-    public int compare(Brand o1, Brand o2) {
-        return o1.name.compareTo(o2.name);
-    }
-}
